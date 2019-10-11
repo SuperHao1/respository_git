af91f09e179fbf4083f57f66114f87f2cae8fd25
SuperHao <729052284@qq.com>
2019-10-11 16:24:25 +0800

    first2 commit git

--- src/Hello/hello_git.java
+++ src/Hello/hello_git.java
@@ -3,6 +3,6 @@ package Hello;
 public class hello_git {
     public static void main(String[] args) {
         System.out.println("hello,git!");
-        
+        System.out.println("hello,git!");
     }
 }
